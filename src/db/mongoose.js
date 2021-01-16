3e99428f3afef5790cec34d93590ca068e92073c
MePilot <nebra2012@gmail.com>
2021-01-16 21:04:59 +0200

    Init

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,7 @@
 
 const mongoose = require('mongoose')
-mongoose.connect('MONGO_DB_URL=mongodb://127.0.0.1:27017/sudoku',{
-    useNewUrlParser:true
-}) 
+mongoose.connect(process.env.MONGO_DB_URL,{useNewUrlParser:true},{useCreateIndex:true}) 
+
 
 
 
